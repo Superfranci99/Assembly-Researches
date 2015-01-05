04bea443b8ebe39fae3b0c3bc465e3f802c66407
Superfranci99 <superfranci99@gmail.com>
2015-01-05 19:13:39 +0100

    Fixed comment position in item.dat

--- Games/Inazuma Eleven 3/item.asm
+++ Games/Inazuma Eleven 3/item.asm
@@ -1,13 +1,13 @@
 ;; Purpose: decrypt item.dat
 
 getBlockPointer:
-	LDR     R1, [SP]           				; R1=offset of the file in the memory
+	LDR     R1, [SP]                        ; R1=offset of the file in the memory
 	MLA     R1, R7, R5, R1                  ; get the pointer of the next block, R1= blockindex * blocklength + fileoffset
-	MOV     R2, R5							; R2 = R5 = block size
-	BL      decodeBlock                     ; Decode Block (R1=offset of the block to decode, R2 = R5 = block size)
-	ADD     R7, R7, #1
+	MOV     R2, R5                          ; R2 = R5 = block size
+	BL      storeEncryptedData              ; Decode Block (R1=offset of the block to decode, R2 = R5 = block size)
+	ADD     R7, R7, #1                      ; increment the counter
 	CMP     R7, #0x400                      ; in item.dat there are 0x400 blocks of 44 bytes, so 45056 bytes total
-	BLT     storeEncryptedData              ; repeat with next block	
+	BLT     getBlockPointer                 ; repeat with next block	
 
 
 storeEncryptedData:
@@ -17,10 +17,10 @@ storeEncryptedData:
 	LDR     R0, [R0]                        ; get the block index (0,1,2,3)
 	MOV     R5, R2
 	ADD     R4, R3, R0,LSL#7                ; get the offset where the data will be stored (due to the block index)
-											; there are 4 slots (128 bytes each)
-											; when all the slots are full, it overrides them from the first
+                                            ; there are 4 slots (128 bytes each)
+                                            ; when all the slots are full, it overrides them from the first
 	MOV     R0, R1
-	MOV     R1, R4							; R1 is the offset where the data block will be stored
+	MOV     R1, R4                          ; R1 is the offset where the data block will be stored
 	MOV     R7, R6
 	BL      getEncryptedData                ; get the data to decrypt and stores it at R1 offset
 	CMP     R5, #0

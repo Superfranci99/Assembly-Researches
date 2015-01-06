4c8e72faa958958a38907a8835bfcb29e67a6689
Superfranci99 <superfranci99@gmail.com>
2015-01-06 12:46:26 +0100

    Finished IE3 item.dat encryprion

--- Games/Inazuma Eleven 3/item.asm
+++ Games/Inazuma Eleven 3/item.asm
@@ -11,6 +11,7 @@ getBlockPointer:
 
 
 storeEncryptedData:
+	STMFD   SP!, {R3-R7,LR}
 	LDR     R0, =dword_20B9DD4              ; load the offset of block index
 	MOV     R6, #0
 	LDR     R3, =dword_20B9DD8              ; load the offset where data starts
@@ -50,8 +51,9 @@ shifing:
 	ADD     R1, R1, #1                      ; R1 is the counter
 	CMP     R1, R5                          ; when the cycle is finished, exit from the cycle
 	BCC     shifing
-	
+	B       doFirstSwap                     ; once shifted all the block, go on
 
+	
 doOperations:
 	MOV     R0, R2,LSL#7                    ; R0 = R2 << 7
 	ORR     R0, R0, R2,ASR#1                ; R0 |= R2 >> 1
@@ -59,3 +61,72 @@ doOperations:
 	CMP     R3, #2                          ; repeats the cycle two times
 	AND     R2, R0, #0xFF                   ; R2 = R0 & 0xFF
 	BLT     doOperations
+
+	
+doFirstSwap:
+	SUBS    R3, R5, #2                      ; the counter of the cycle will arrive to blocklength - 2
+	MOV     R7, #0                          ; R7 counter
+	BEQ     doSecondSwap                    ; exit
+	ADD     R2, R4, R7                      ; first swap cycle
+	LDRB    R1, [R4,R7]                     ; read the data and recalculate the pointer AGAIN
+	LDRB    R0, [R2,#2]                     ; skip 2 bytes (including the one just read)
+	STRB    R0, [R4,R7]
+	ADD     R7, R7, #3                      ; increment the counter R7 of 3
+	STRB    R1, [R2,#2]                     ; swap the 2 bytes    ABC --> CBA don't change B
+	CMP     R7, R3
+	BCC     doFirstSwap                     ; swap again
+	B       doSecondSwap
+
+	
+doSecondSwap:
+	SUBS    R3, R5, #4                      ; the counter of the cycle will arrive to blocklength - 4
+	MOV     R7, #0                          ; R7 counter
+	BEQ     doThirdSwap                     ; exit
+	ADD     R2, R4, R7                      ; second swap cycle
+	LDRB    R1, [R4,R7]
+	LDRB    R0, [R2,#4]                     ; ABCDE --> EBCDA
+	STRB    R0, [R4,R7]
+	ADD     R7, R7, #5
+	STRB    R1, [R2,#4]
+	CMP     R7, R3
+	BCC     doSecondSwap                    ; second swap cycle
+	B       doThirdSwap
+	
+doThirdSwap:
+	SUBS    R3, R5, #6                      ; the counter of the cycle will arrive to blocklength - 6
+	MOV     R7, #0                          ; R7 counter
+	BEQ     doFourthSwap                    ; initialize the fourth cycle
+	ADD     R2, R4, R7                      ; third swap cycle
+	LDRB    R1, [R4,R7]
+	LDRB    R0, [R2,#6]                     ; ABCDEFG --> GBCDEFA
+	STRB    R0, [R4,R7]
+	ADD     R7, R7, #7
+	STRB    R1, [R2,#6]
+	CMP     R7, R3
+	BCC     doThirdSwap                     ; third swap cycle
+	B       doFourthSwap
+
+	
+doFourthSwap:
+	SUBS    R3, R5, #1                      ; initialize the fourth cycle
+	MOV     R5, #0
+	BEQ     modifyBlockIndex                ; get the actual block index
+	ADD     R2, R4, R5                      ; fourth swap cycle
+	LDRB    R1, [R4,R5]
+	LDRB    R0, [R2,#1]
+	STRB    R0, [R4,R5]
+	ADD     R5, R5, #2
+	STRB    R1, [R2,#1]                     ; ABCDEF --> BADCFE
+	CMP     R5, R3
+	BCC     doFourthSwap                    ; fourth swap cycle
+
+
+modifyBlockIndex:
+	LDR     R0, =dword_20B9DD4              ; get the actual block index
+	LDR     R1, [R0]                        ; ...
+	ADD     R1, R1, #1                      ; increment the index
+	STR     R1, [R0]                        ; store the new value
+	CMP     R1, #4                          ; if it's out of range (0-3) reset it to zero
+	STREQ   R6, [R0]                        ; ... if it's zero, store it
+	MOV     R0, R4
+	LDMFD   SP!, {R3-R7,PC}                 ; getBlockPointer
